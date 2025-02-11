75b320c48fadee859581164c0d19997ebd89792a
Banxoz <banxoc@gmail.com>
2025-02-11 22:48:56 +0300

    Add files via upload

---
+++ АВС_Овсянников_Никита_И-1-24_ЛР_№1_В_8.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <bitset>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int dec;
+    int go;
+    cout << "Введите число в десятичной системе счисления: ";
+    cin >> dec;
+    cout << "Двоичный вид: " << bitset<8>(dec) << endl;
+    int i = 1;
+    while (i >= 1)
+    {
+        cout << "Выполнить побитовый сдвиг влево? Введите 1, если да: ";
+        cin >> go;
+        if (go == 1)
+        {
+            dec = dec * 2;
+            cout << "Двоичный вид: " << bitset<16>(dec) << endl;
+        }
+        else {
+            i = 0;
+            cout << "Стоп машина!";
+        }
+    }
+
+    return 0;
+}
